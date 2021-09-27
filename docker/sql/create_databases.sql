97941edc23ec1325e5b20a1ed14354fb6430e2d3
Ryan Roberts <ryan@blockchaintp.com>
2021-09-27 12:36:54 +0100

    fix: remove additional urls from docker images
    Signed-off-by: Ryan Roberts <ryan@blockchaintp.com>

--- docker/sql/create_databases.sql
+++ docker/sql/create_databases.sql
@@ -1,8 +1,4 @@
-create database qldb_damlstate;
-grant all privileges on database qldb_damlstate to postgres;
 create database qldb_txblobs;
 grant all privileges on database qldb_txblobs to postgres;
-create database pg_damlstate;
-grant all privileges on database pg_damlstate to postgres;
 create database pg_txblobs;
 grant all privileges on database pg_txblobs to postgres;
